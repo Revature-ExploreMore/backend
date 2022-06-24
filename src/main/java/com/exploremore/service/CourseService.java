eb5645cc786f2dd3bcbe1663087c7f2069a663df
navdeep2808 <navdeep.kr21@gmail.com>
2022-06-24 14:29:37 -0400

    Course Controller Started

--- src/main/java/com/exploremore/service/CourseService.java
+++ src/main/java/com/exploremore/service/CourseService.java
@@ -1,5 +1,18 @@
 package com.exploremore.service;
 
+import java.util.List;
+
+import com.exploremore.pojo.CoursePojo;
+
 public interface CourseService {
+
+	CoursePojo addCourse(CoursePojo coursePojo);
+
+	List<CoursePojo> getAllCourses();
+
+	CoursePojo getACourseById(int id);
+
+	CoursePojo getACourseByCategory(String name);
+
 
 }
