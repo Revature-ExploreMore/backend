e9e0dacc9e564db87a6e12d9efacc5f812f5746d
Isaac Suttle <104944219+mazinskater@users.noreply.github.com>
2022-06-27 11:02:30 -0400

    Fixed errors in cart

--- src/main/java/com/exploremore/controller/CartController.java
+++ src/main/java/com/exploremore/controller/CartController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.exploremore.exceptions.GlobalException;
 import com.exploremore.pojo.CartCoursePojo;
 import com.exploremore.service.CartService;
 
@@ -21,15 +20,15 @@ public class CartController {
 	@Autowired
 	CartService cartService;
 	
-//	@GetMapping("/cartcourse/{cart_id}")
-//	public List<CartCoursePojo> getCartCoursesByCart(@PathVariable("cart_id") int id) {
-//		try {
-//			return cartService.getCartCourses(id);
-//		} catch (GlobalException e) {
-//			// TODO Auto-generated catch block						** please do not merge when errors are present
-//			e.printStackTrace();										recommend to use built-in exception until 
-//			return null;                                                the global exception is configured; make a comment 
-//		}                                                               of the necessary global exception
-//	}
+	@GetMapping("cartcourse/{cid}")
+	public List<CartCoursePojo> getCartCoursesByCart(@PathVariable("cid") int id) {
+		//try {
+			return cartService.getCartCourses(id);
+		//} catch (GlobalException e) {
+			// TODO Auto-generated catch block						** please do not merge when errors are present
+			//e.printStackTrace();										//recommend to use built-in exception until 
+			//return null;                                              //  the global exception is configured; make a comment 
+		//}                                                             //  of the necessary global exception
+	}
 	
 }
--- src/main/java/com/exploremore/dao/CartDao.java
+++ src/main/java/com/exploremore/dao/CartDao.java
@@ -2,12 +2,14 @@ package com.exploremore.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.exploremore.entity.CartCourseEntity;
 
+
 @Repository
-public interface CartDao {
+public interface CartDao extends JpaRepository<CartCourseEntity, Integer> {
 	List<CartCourseEntity> findByCartId(int cart_id);
 
 }
