1d6431c85ef4f93072c5400c325a32533abc0c9c
berlyPaul360 <91963846+berlyPaul360@users.noreply.github.com>
2022-07-01 18:02:21 -0400

    get payment returns list

--- src/main/java/com/exploremore/controller/PaymentController.java
+++ src/main/java/com/exploremore/controller/PaymentController.java
@@ -1,6 +1,9 @@
 package com.exploremore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.exploremore.exceptions.GlobalException;
 import com.exploremore.pojo.PaymentPojo;
 import com.exploremore.service.PaymentService;
+
+
 
 @CrossOrigin
 @RestController
@@ -29,7 +34,7 @@ public class PaymentController {
 	}
 	
 	@GetMapping("payment/{uid}")
-	public PaymentPojo getPaymentInfo(@PathVariable("uid") int userId) throws GlobalException{
+	public List<PaymentPojo> getPaymentInfo(@PathVariable("uid") int userId) throws GlobalException{
 		return paymentService.getPaymentInfo(userId);
 	}
 }
