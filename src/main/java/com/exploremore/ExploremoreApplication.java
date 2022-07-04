4d0b9cdf3c1e0b3b71bf0a5867e86e93afb4c03f
MaganaOscar <oscmag13@gmail.com>
2022-07-04 10:28:09 -0700

    bcrypt functionality

--- src/main/java/com/exploremore/ExploremoreApplication.java
+++ src/main/java/com/exploremore/ExploremoreApplication.java
@@ -5,12 +5,13 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ExploremoreApplication {
 	@Bean
     public CorsFilter corsFilter() {
